532649f2e3e4f5b844bbf7d3f6f266845169b191
rasyidcode7 <rasyidcode7@gmail.com>
2018-07-13 10:47:21 +0700

    error nomnom

--- map_research/nomnom/lib/main.dart
+++ map_research/nomnom/lib/main.dart
@@ -53,7 +53,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  MapView mapview = MapView();
+  MapView mapview = new MapView();
   var compositeSubscription = CompositeSubscription();
 
   @override
