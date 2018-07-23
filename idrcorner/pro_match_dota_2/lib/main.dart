5d6ea544b3e3ca8d9bd34fc4404a55d88b6213d5
rasyidcode7 <rasyidcode7@gmail.com>
2018-07-23 10:57:15 +0700

    completed issue #9

--- idrcorner/pro_match_dota_2/lib/main.dart
+++ idrcorner/pro_match_dota_2/lib/main.dart
@@ -13,10 +13,7 @@ void main() {
     MaterialApp(
       title: 'Pro Match Dota 2',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        accentColor: Colors.redAccent
-      ),
+      theme: ThemeData.dark(),
       home: Home(),
     )
   );
@@ -45,127 +42,137 @@ class _HomeState extends State<Home> {
       this._getMatch();
     }
 
-  Widget _matchItem2(int index) {
-    var format = DateFormat("d M y");
-    String secondEpochTimeStamp = dataMatch[index]['start_time'].toString() + "000";
-    int timestamp = int.parse(secondEpochTimeStamp);
-
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text('ID: '+dataMatch[index]['match_id'].toString()),
-          Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text('Team Name'),
-                  Text('Score')
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Text('Radiant'),
-                      Text('Dire'),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(dataMatch[index]['radiant_name'] == null ? 'Unknown Team Name' : dataMatch[index]['radiant_name']),
-                      Text(dataMatch[index]['dire_name'] == null ? 'Unknown Team Name' : dataMatch[index]['dire_name']),
-                    ],
-                  )
-                ],
-              )
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //   children: <Widget>[
-              //     Text('Radiant'),
-              //     Text(dataMatch[index]['radiant_name'] == null ? 'Unknown Team Name' : dataMatch[index]['radiant_name']),
-              //     Text(dataMatch[index]['radiant_score'].toString())
-              //   ],
-              // ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //   children: <Widget>[
-              //     Text('Dire'),
-              //     Text(dataMatch[index]['dire_name'] == null ? 'Unknown Team Name' : dataMatch[index]['dire_name']),
-              //     Text(dataMatch[index]['dire_score'].toString())
-              //   ],
-              // )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
   Widget _matchItem(int index) {
-    var format = DateFormat("d M y");
+
+    var format = DateFormat("d/M/y h:m:s");
     String secondEpochTimeStamp = dataMatch[index]['start_time'].toString() + "000";
     int timestamp = int.parse(secondEpochTimeStamp);
-    //print(timestamp);
-    print(dataMatch[index]['radiant_name']);
 
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(),
-        borderRadius: BorderRadius.circular(12.0)
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: dataMatch[index]['radiant_win'] ? 
+              Colors.green.withOpacity(0.3) : 
+              Colors.red.withOpacity(0.3),
+            offset: Offset(1.0, 1.0),
+            blurRadius: 1.0
+          )
+        ]
       ),
+      padding: const EdgeInsets.all(12.0),
+      margin: const EdgeInsets.only(bottom: 10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text('ID: '+dataMatch[index]['match_id'].toString()),
-          Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                color: dataMatch[index]['radiant_win'] ? Colors.green : Colors.red,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Text('Radiant'),
-                        Text(dataMatch[index]['radiant_name'] == null ? 'Unknown Team Name' : dataMatch[index]['radiant_name'])
-                      ],
-                    ),
-                    Text(dataMatch[index]['radiant_score'].toString())
-                  ],
-                ),
-              ),
-              Container(
-                color: dataMatch[index]['radiant_win'] ? Colors.red : Colors.green,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Text('Dire'),
-                        Text(dataMatch[index]['radiant_name'] == null ? 'Unknown Team Name' : dataMatch[index]['radiant_name'])
-                      ],
-                    ),
-                    Text(dataMatch[index]['dire_score'].toString())
-                  ],
-                )
-              )
-            ],
+          Text(
+            dataMatch[index]['league_name'],
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16.0,
+            ),
           ),
           Divider(),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Text(format.format(DateTime.fromMillisecondsSinceEpoch(timestamp))),
-              Text(dataMatch[index]['league_name'])
+              Text(
+                'ID: '+dataMatch[index]['match_id'].toString(),
+                style: TextStyle(
+                  fontSize: 9.0
+                ),
+              ),
+              Text(
+                format.format(DateTime.fromMillisecondsSinceEpoch(timestamp)),
+                style: TextStyle(
+                  fontSize: 9.0
+                ),
+              )
             ],
+          ),
+          Container(height: 8.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    'Team Name',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                  Container(height: 6.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Radiant',
+                            style: TextStyle(color: Colors.green, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            'Dire',
+                            style: TextStyle(color: Colors.red, fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                      Container(width: 20.0),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(dataMatch[index]['radiant_name'] == null ? 'Unknown Team Name' : dataMatch[index]['radiant_name']),
+                          Text(dataMatch[index]['dire_name'] == null ? 'Unknown Team Name' : dataMatch[index]['dire_name']),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Text(
+                    'Score',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                  Container(height: 6.0),
+                  Text(
+                    dataMatch[index]['radiant_score'].toString(),
+                    style: TextStyle(
+                      color: dataMatch[index]['radiant_score'] > dataMatch[index]['dire_score'] ?
+                        Colors.green : Colors.red,
+                        fontWeight: FontWeight.w900
+                    ),
+                  ),
+                  Text(
+                    dataMatch[index]['dire_score'].toString(),
+                    style: TextStyle(
+                      color: dataMatch[index]['radiant_score'] > dataMatch[index]['dire_score'] ?
+                        Colors.red : Colors.green,
+                      fontWeight: FontWeight.w900
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+          Divider(),
+          Text(
+            dataMatch[index]['radiant_win'] ? 'RADIANT WIN' : 'DIRE WIN',
+            style: TextStyle(
+              color: dataMatch[index]['radiant_win'] ? Colors.green : Colors.red
+            ),
+            textAlign: TextAlign.center,
           )
         ],
-      )
+      ),
     );
   }
 
@@ -175,10 +182,12 @@ class _HomeState extends State<Home> {
       return Scaffold(
         appBar: AppBar(
           title: Text('Pro Match Dota 2'),
+          centerTitle: true,
         ),
         body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) => _matchItem2(index),
+          itemBuilder: (BuildContext context, int index) => _matchItem(index),
           itemCount: dataMatch == null ? 0 : dataMatch.length,
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
         ),
       );
     }
